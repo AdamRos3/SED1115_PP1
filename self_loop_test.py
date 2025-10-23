4cc3cb7b032150bfc346a2f651d865d4b8c542e0
Adam R <adam@rosenblatt.ca>
2025-10-23 11:20:32 -0400

    removed uart to purely test PWM

--- self_loop_test.py
+++ self_loop_test.py
@@ -1,11 +1,8 @@
-from machine import UART, Pin, ADC, PWM
+from machine import Pin, ADC, PWM
 import time
 
 test_button = Pin(22, Pin.IN, Pin.PULL_DOWN)
 prev_state = 0
-
-uart = UART(1, baudrate=9600, tx=Pin(8), rx=Pin(9))
-uart.init(bits=8, parity=None, stop=1) 
 
 adc = ADC(Pin(27))
 pwm = PWM(Pin(0), freq=2000) 
@@ -18,26 +15,18 @@ pwm.duty_u16(duty_cycle)
 
 while True:
     current_state = test_button.value()
-        
+    average_pwm = (float(adc.read_u16()) / 65535) * 3.3
+
     if (prev_state == 0 and current_state == 1):
-        message = str(desired_value) + '\n'
-        uart.write(message)
+        diff = abs(desired_value - average_pwm)
+
+        print("Desired: " + str(desired_value))
+        print("Actual: " + str(average_pwm))
+        print("Difference: " + str(diff))
+        print()
+
 
         time.sleep(0.3) # Debounce delay
-    
-    if uart.any():
-        data = uart.read()
-        if data:
-            data = data.decode('utf-8').strip()
-            average_pwm = (float(adc.read_u16()) / 65535) * 3.3
-            diff = abs(float(data) - average_pwm)
-
-            print("Desired: " + data)
-            print("Actual: " + str(average_pwm))
-            print("Difference: " + str(diff))
-            print()
-
-
 
     prev_state = current_state
 
