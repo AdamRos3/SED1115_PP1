68482ee50f45d65be3a0539ea7b65198c248026a
Adam R <adam@rosenblatt.ca>
2025-10-25 05:02:49 -0400

    loop back test completed, additional file for reading from external ADC aquired

--- self_loop_test.py
+++ self_loop_test.py
@@ -1,33 +1,36 @@
-from machine import Pin, ADC, PWM
+# Note: for this to properly be imported, the ads1x15.py file must be loaded onto the pico
+from ads1x15 import ADS1015
+from machine import Pin, ADC, PWM, I2C
 import time
 
-test_button = Pin(22, Pin.IN, Pin.PULL_DOWN)
-prev_state = 0
+# I2C signal pins
+I2C_SDA = 14
+I2C_SCL = 15
 
-adc = ADC(Pin(27))
-pwm = PWM(Pin(0), freq=2000) 
+# PWM generating pin
+PWM_GENERATOR = 0
+
+# ADS 1015 configuration information
+ADS1015_ADDR = 0x48
+ADS1015_PWM = 2     # port 2 has (low-pass filtered) PWM signal
+
+i2c = I2C(1, sda=Pin(I2C_SDA), scl=Pin(I2C_SCL))
+adc = ADS1015(i2c, ADS1015_ADDR, 1)
+pwm = PWM(Pin(PWM_GENERATOR), freq=10000) 
 
 duty_cycle = 32768
 desired_value = (duty_cycle / 65535) * 3.3
 
 pwm.duty_u16(duty_cycle) 
 
-
 while True:
-    current_state = test_button.value()
-    average_pwm = (float(adc.read_u16()) / 65535) * 3.3
+    average_pwm = adc.raw_to_v(adc.read(0, ADS1015_PWM))
 
-    if (prev_state == 0 and current_state == 1):
-        diff = abs(desired_value - average_pwm)
+    diff = abs(desired_value - average_pwm)
 
-        print("Desired: " + str(desired_value))
-        print("Actual: " + str(average_pwm))
-        print("Difference: " + str(diff))
-        print()
+    print("Desired: " + str(desired_value) + "V")
+    print("Actual: " + str(average_pwm) + "V")
+    print("Difference: " + str(diff) + "V")
+    print()
 
-
-        time.sleep(0.3) # Debounce delay
-
-    prev_state = current_state
-
-    time.sleep(0.1)
+    time.sleep(1)
